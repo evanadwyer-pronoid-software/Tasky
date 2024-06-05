dc1d22bcb77c56662554b59044c847a6aad44b76
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-05 09:27:28 -0700

    Create Build Logic for convention plugins. Add dependencies to version catalog (#7)

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.pronoidsoftware.tasky.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+
+    }
+}
---
+++ build-logic/convention/src/main/java/com/pronoidsoftware/convention/BuildTypes.kt
@@ -0,0 +1,71 @@
+package com.pronoidsoftware.convention
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.BuildType
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+internal fun Project.configureBuildTypes(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+    extensionType: ExtensionType,
+) {
+    commonExtension.run {
+        buildFeatures {
+            buildConfig = true
+        }
+
+        val apiKey = gradleLocalProperties(rootDir, rootProject.providers).getProperty("API_KEY")
+        val baseUrl = gradleLocalProperties(rootDir, rootProject.providers).getProperty("BASE_URL")
+        when (extensionType) {
+            ExtensionType.APPLICATION -> {
+                extensions.configure<ApplicationExtension> {
+                    buildTypes {
+                        debug {
+                            configureDebugBuildType(apiKey, baseUrl)
+                        }
+                        release {
+                            configureReleaseBuildType(commonExtension, apiKey, baseUrl)
+                        }
+                    }
+                }
+            }
+
+            ExtensionType.LIBRARY -> {
+                extensions.configure<LibraryExtension> {
+                    buildTypes {
+                        debug {
+                            configureDebugBuildType(apiKey, baseUrl)
+                        }
+                        release {
+                            configureReleaseBuildType(commonExtension, apiKey, baseUrl)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+private fun BuildType.configureDebugBuildType(apiKey: String, baseUrl: String) {
+    buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
+}
+
+private fun BuildType.configureReleaseBuildType(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+    apiKey: String,
+    baseUrl: String,
+) {
+    buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
+
+    isMinifyEnabled = true
+    proguardFiles(
+        commonExtension.getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+    )
+}
