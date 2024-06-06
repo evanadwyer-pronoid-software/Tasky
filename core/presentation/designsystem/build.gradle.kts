7e70c522d82799376ed7087f898277b97f4f74d8
evanadwyer <evanadwyer@pronoid-software.com>
2024-06-06 09:10:16 -0700

    Introduce colors

--- core/presentation/designsystem/build.gradle.kts
+++ core/presentation/designsystem/build.gradle.kts
@@ -7,4 +7,11 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    api(libs.androidx.compose.material3)
 }
