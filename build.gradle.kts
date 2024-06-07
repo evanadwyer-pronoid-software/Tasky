6710994601301b67b118d18fe4fea91ec0d46a1a
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-07 09:46:27 -0700

    upgrade to Kotlin 2.0.0 (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
