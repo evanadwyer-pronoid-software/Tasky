ae1c5c5d919483ffe51fc0d9e1f0a83a5dc30190
evanadwyer <evanadwyer@pronoid-software.com>
2024-06-08 10:36:57 -0700

    Create UiText

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -9,4 +9,11 @@ android {
 dependencies {
     implementation(projects.core.domain)
     implementation(projects.core.presentation.designsystem)
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 }
