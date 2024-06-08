9cbae8b7e6975e6d09af97d855251ce115049f60
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-08 10:32:05 -0700

    Create Error handling wrapper (#17)

---
+++ core/domain/src/main/java/com/pronoidsoftware/core/domain/util/Result.kt
@@ -0,0 +1,20 @@
+package com.pronoidsoftware.core.domain.util
+
+sealed interface Result<out D, out E : Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+    data class Error<out E : com.pronoidsoftware.core.domain.util.Error>(val error: E) :
+        Result<Nothing, E>
+}
+
+inline fun <T, E : Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+    return when (this) {
+        is Result.Error -> Result.Error(error)
+        is Result.Success -> Result.Success(map(data))
+    }
+}
+
+fun <T, E : Error> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
+    return map { }
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
