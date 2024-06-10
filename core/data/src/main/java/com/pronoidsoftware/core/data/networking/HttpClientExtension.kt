2dc171cb67fe6dd1a84996ca3d620c76cb5913b3
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-10 12:34:22 -0700

    Create Http client extensions for HTTP requests (#20)

---
+++ core/data/src/main/java/com/pronoidsoftware/core/data/networking/HttpClientExtension.kt
@@ -0,0 +1,135 @@
+package com.pronoidsoftware.core.data.networking
+
+import com.pronoidsoftware.core.data.BuildConfig
+import com.pronoidsoftware.core.domain.util.DataError
+import com.pronoidsoftware.core.domain.util.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.CancellationException
+import kotlinx.serialization.SerializationException
+
+suspend inline fun <reified Response : Any> HttpClient.get(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf(),
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        get {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.delete(
+    route: String,
+    queryParameters: Map<String, Any?> = mapOf(),
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        delete {
+            url(constructRoute(route))
+            queryParameters.forEach { (key, value) ->
+                parameter(key, value)
+            }
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.post(
+    route: String,
+    body: Request,
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        post {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.postMultipart(
+    route: String,
+    body: MultiPartFormDataContent,
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        post {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.put(
+    route: String,
+    body: Request,
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        put {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.putMultipart(
+    route: String,
+    body: MultiPartFormDataContent,
+): Result<Response, DataError.Network> {
+    return safeCAll {
+        put {
+            url(constructRoute(route))
+            setBody(body)
+        }
+    }
+}
+
+suspend inline fun <reified T> safeCAll(execute: () -> HttpResponse): Result<T, DataError.Network> {
+    val response = try {
+        execute()
+    } catch (e: UnresolvedAddressException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.NO_INTERNET)
+    } catch (e: SerializationException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.SERIALIZATION)
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        e.printStackTrace()
+        return Result.Error(DataError.Network.UNKNOWN)
+    }
+
+    return responseToResult(response)
+}
+
+suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Network> {
+    return when (response.status.value) {
+        in 200..299 -> Result.Success(response.body<T>())
+        401 -> Result.Error(DataError.Network.UNAUTHORIZED)
+        408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
+        409 -> Result.Error(DataError.Network.CONFLICT)
+        413 -> Result.Error(DataError.Network.PAYLOAD_TOO_LARGE)
+        429 -> Result.Error(DataError.Network.TOO_MANY_REQUESTS)
+        in 500..599 -> Result.Error(DataError.Network.SERVER_ERROR)
+        else -> Result.Error(DataError.Network.UNKNOWN)
+    }
+}
+
+fun constructRoute(route: String): String {
+    return when {
+        route.contains(BuildConfig.BASE_URL) -> route
+        route.startsWith("/") -> BuildConfig.BASE_URL + route
+        else -> BuildConfig.BASE_URL + "/$route"
+    }
+}
