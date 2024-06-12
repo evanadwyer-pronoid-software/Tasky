cc006703e9a72517dd3d725c4f7d21e941ec938e
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-12 15:05:03 -0700

    Create Hilt Gradle Convention Plugins (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
