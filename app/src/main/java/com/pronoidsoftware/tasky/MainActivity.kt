fe15408e86017ed64f77078614f13c1a367c792a
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-06-17 15:53:37 -0700

    Finish hilt setup (#29)

--- app/src/main/java/com/pronoidsoftware/tasky/MainActivity.kt
+++ app/src/main/java/com/pronoidsoftware/tasky/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.pronoidsoftware.core.presentation.designsystem.TaskyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
