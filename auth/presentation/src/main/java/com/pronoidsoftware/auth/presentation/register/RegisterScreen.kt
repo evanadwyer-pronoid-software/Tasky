b190a4d7a7ff7ee65ad7c1d1ad460a9636731b08
evanadwyer <evanadwyer@pronoid-software.com>
2024-06-22 18:32:57 -0700

    disable action button while loading so that it cannot be spammed

--- auth/presentation/src/main/java/com/pronoidsoftware/auth/presentation/register/RegisterScreen.kt
+++ auth/presentation/src/main/java/com/pronoidsoftware/auth/presentation/register/RegisterScreen.kt
@@ -183,7 +183,7 @@ internal fun RegisterScreen(state: RegisterState, onAction: (RegisterAction) -> 
                 isLoading = state.isRegistering,
                 modifier = Modifier.fillMaxWidth(),
                 onClick = { onAction(RegisterAction.OnRegisterClick) },
-                enabled = state.canRegister.value || state.isRegistering,
+                enabled = state.canRegister.value,
             )
         }
     }
