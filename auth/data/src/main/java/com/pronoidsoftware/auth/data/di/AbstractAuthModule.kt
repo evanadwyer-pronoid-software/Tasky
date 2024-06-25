ee5f2d9acbb13cd2ea2fac36a5b7f80178f2d07b
evanadwyer <evanadwyer@pronoid-software.com>
2024-06-25 09:38:26 -0700

    rename AuthRepositoryModule.kt to AbstractAuthModule

--- auth/data/src/main/java/com/pronoidsoftware/auth/data/di/AbstractAuthModule.kt
+++ auth/data/src/main/java/com/pronoidsoftware/auth/data/di/AbstractAuthModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AuthRepositoryModule {
+abstract class AbstractAuthModule {
 
     @Binds
     @Singleton
