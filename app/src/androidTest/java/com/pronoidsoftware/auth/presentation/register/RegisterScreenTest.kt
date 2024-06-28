30097a76057f203fab55f2c2000a03b66199148e
evanadwyer <evanadwyer@pronoid-software.com>
2024-06-28 10:36:54 -0700

    fix ui tests for login screen and register screen

--- app/src/androidTest/java/com/pronoidsoftware/auth/presentation/register/RegisterScreenTest.kt
+++ app/src/androidTest/java/com/pronoidsoftware/auth/presentation/register/RegisterScreenTest.kt
@@ -2,11 +2,9 @@ package com.pronoidsoftware.auth.presentation.register
 
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.navigation.compose.rememberNavController
 import com.pronoidsoftware.core.TaskyAndroidTest
 import com.pronoidsoftware.core.presentation.designsystem.TaskyTheme
 import com.pronoidsoftware.tasky.MainActivity
-import com.pronoidsoftware.tasky.NavigationRoot
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
@@ -21,36 +19,37 @@ class RegisterScreenTest : TaskyAndroidTest() {
     fun testRegisterScreenUi_allValid() {
         composeRule.activity.setContent {
             TaskyTheme {
-                val navController = rememberNavController()
-                NavigationRoot(navController = navController)
-                RegisterScreenRobot(composeRule)
-                    .navigateTo(navController)
-                    .assertRegisterButtonIsDisabled()
-                    .assertNameIsInvalid()
-                    .clickName()
-                    .inputText("Val")
-                    .assertNameIsInvalid()
-                    .inputText("Valid Name")
-                    .assertNameIsValid()
-                    .assertRegisterButtonIsDisabled()
-                    .assertEmailIsInvalid()
-                    .clickEmail()
-                    .inputText("example")
-                    .assertEmailIsInvalid()
-                    .inputText("example@example.com")
-                    .assertEmailIsValid()
-                    .assertRegisterButtonIsDisabled()
-                    .assertPasswordIsInvalid()
-                    .clickPassword()
-                    .inputText("Invalid1")
-                    .assertPasswordIsInvalid()
-                    .inputText("Test12345")
-                    .assertPasswordIsValid()
-                    .showPassword()
-                    .hidePassword()
-                    .assertRegisterButtonIsEnabled()
-                    .clickRegisterButton()
+                RegisterScreenRoot(
+                    onLogInClick = { },
+                    onSuccessfulRegistration = { },
+                )
             }
         }
+        RegisterScreenRobot(composeRule)
+            .assertRegisterButtonIsDisabled()
+            .assertNameIsInvalid()
+            .clickName()
+            .inputText("Val")
+            .assertNameIsInvalid()
+            .inputText("Valid Name")
+            .assertNameIsValid()
+            .assertRegisterButtonIsDisabled()
+            .assertEmailIsInvalid()
+            .clickEmail()
+            .inputText("example")
+            .assertEmailIsInvalid()
+            .inputText("example@example.com")
+            .assertEmailIsValid()
+            .assertRegisterButtonIsDisabled()
+            .assertPasswordIsInvalid()
+            .clickPassword()
+            .inputText("Invalid1")
+            .assertPasswordIsInvalid()
+            .inputText("Test12345")
+            .assertPasswordIsValid()
+            .showPassword()
+            .hidePassword()
+            .assertRegisterButtonIsEnabled()
+            .clickRegisterButton()
     }
 }
