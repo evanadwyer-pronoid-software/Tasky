1d1c8718e2833cf31fa99bc9563f1ab0e36141d8
evanadwyer <evanadwyer@pronoid-software.com>
2024-07-10 10:03:11 -0700

    utility for initials and capitalization

---
+++ core/domain/src/main/java/com/pronoidsoftware/core/domain/util/FullNameToInitials.kt
@@ -0,0 +1,39 @@
+package com.pronoidsoftware.core.domain.util
+
+fun String.toInitials(): String {
+    val words = this.split(" ")
+    return when (words.size) {
+        0 -> "" // should not happen
+        1 -> {
+            getCharacters(words.first(), 2)
+        }
+
+        else -> {
+            val first = words.first()
+            val last = words.last()
+            "${getCharacters(first, 1)}${getCharacters(last, 1)}"
+        }
+    }
+}
+
+private fun getCharacters(word: String, need: Int): String {
+    val result = StringBuilder()
+    var index = 0
+    var count = 0
+    while (index < word.length && count < need) {
+        if (word[index].isHighSurrogate() &&
+            index + 1 < word.length &&
+            word[index + 1].isLowSurrogate()
+        ) {
+            // This is a surrogate pair (emoji), add it as a single unit
+            result.append(word[index]).append(word[index + 1])
+            index += 2 // Skip the next char as it's part of the emoji
+        } else {
+            // This is a regular character, add it
+            result.append(word[index])
+            index++
+        }
+        count++
+    }
+    return result.toString()
+}
