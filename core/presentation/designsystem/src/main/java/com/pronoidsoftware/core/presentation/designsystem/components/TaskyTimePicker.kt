8f8e62e9719c5242691607652227a3c47471a7c9
evanadwyer <evanadwyer@pronoid-software.com>
2024-07-17 09:56:04 -0700

    Make Time Picker dialog look like Date picker dialog

--- core/presentation/designsystem/src/main/java/com/pronoidsoftware/core/presentation/designsystem/components/TaskyTimePicker.kt
+++ core/presentation/designsystem/src/main/java/com/pronoidsoftware/core/presentation/designsystem/components/TaskyTimePicker.kt
@@ -2,39 +2,38 @@
 
 package com.pronoidsoftware.core.presentation.designsystem.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerDefaults
 import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
+import com.pronoidsoftware.core.domain.util.now
 import com.pronoidsoftware.core.presentation.designsystem.R
+import com.pronoidsoftware.core.presentation.designsystem.TaskyBlack
+import com.pronoidsoftware.core.presentation.designsystem.TaskyGreen
+import com.pronoidsoftware.core.presentation.designsystem.TaskyLightGray
+import com.pronoidsoftware.core.presentation.designsystem.TaskyWhite
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalTime
 
 @Composable
 fun TaskyTimePicker(
     selectedTime: LocalTime,
+    onSelectTime: (LocalTime) -> Unit,
     expanded: Boolean,
     toggleExpanded: () -> Unit,
-    onSelectTime: (LocalTime) -> Unit,
     modifier: Modifier = Modifier,
+    clock: Clock = Clock.System,
     content: @Composable () -> Unit,
 ) {
     val timePickerState = TimePickerState(
@@ -45,73 +44,56 @@ fun TaskyTimePicker(
 
     content()
     if (expanded) {
-        TimePickerDialog(
+        // TimePickerDialog not shipped with Material3...
+        // https://issuetracker.google.com/issues/288311426
+        DatePickerDialog(
             modifier = modifier,
-            onCancel = toggleExpanded,
-            onConfirm = {
-                toggleExpanded()
-                onSelectTime(LocalTime(timePickerState.hour, timePickerState.minute))
+            onDismissRequest = toggleExpanded,
+            confirmButton = {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    TextButton(
+                        onClick = {
+                            toggleExpanded()
+                            onSelectTime(
+                                now(clock = clock).time,
+                            )
+                        },
+                    ) {
+                        Text(text = stringResource(id = R.string.now))
+                    }
+                    TextButton(onClick = toggleExpanded) {
+                        Text(text = stringResource(id = R.string.cancel))
+                    }
+                    TextButton(
+                        onClick = {
+                            toggleExpanded()
+                            onSelectTime(LocalTime(timePickerState.hour, timePickerState.minute))
+                        },
+                    ) {
+                        Text(text = stringResource(id = R.string.confirm))
+                    }
+                }
             },
         ) {
-            TimePicker(state = timePickerState)
-        }
-    }
-}
-
-// not shipped with Material3...
-// https://issuetracker.google.com/issues/288311426
-@Composable
-private fun TimePickerDialog(
-    onCancel: () -> Unit,
-    onConfirm: () -> Unit,
-    modifier: Modifier = Modifier,
-    title: String = "Select Time",
-    toggle: @Composable () -> Unit = {},
-    content: @Composable () -> Unit,
-) {
-    Dialog(
-        onDismissRequest = onCancel,
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false,
-        ),
-    ) {
-        Surface(
-            shape = MaterialTheme.shapes.extraLarge,
-            tonalElevation = 6.dp,
-            modifier = modifier
-                .width(IntrinsicSize.Min)
-                .height(IntrinsicSize.Min)
-                .background(
-                    shape = MaterialTheme.shapes.extraLarge,
-                    color = MaterialTheme.colorScheme.surface,
-                ),
-        ) {
-            toggle()
-            Column(
-                modifier = Modifier.padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Center,
             ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 20.dp),
-                    text = title,
-                    style = MaterialTheme.typography.labelMedium,
+                TimePicker(
+                    state = timePickerState,
+                    colors = TimePickerDefaults.colors().copy(
+                        clockDialColor = TaskyLightGray,
+                        containerColor = TaskyWhite,
+                        periodSelectorSelectedContainerColor = TaskyGreen,
+                        timeSelectorUnselectedContainerColor = TaskyLightGray,
+                        timeSelectorSelectedContainerColor = TaskyGreen,
+                        periodSelectorSelectedContentColor = TaskyWhite,
+                        periodSelectorUnselectedContentColor = TaskyBlack,
+                        periodSelectorUnselectedContainerColor = TaskyLightGray,
+                    ),
                 )
-                content()
-                Row(
-                    modifier = Modifier
-                        .height(40.dp)
-                        .fillMaxWidth(),
-                ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    TextButton(
-                        onClick = onCancel,
-                    ) { Text(stringResource(id = R.string.cancel)) }
-                    TextButton(
-                        onClick = onConfirm,
-                    ) { Text(stringResource(id = R.string.confirm)) }
-                }
             }
         }
     }
