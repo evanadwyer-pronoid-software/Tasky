2749f75744e07ca7b36a74bfe1b1d92c9d183401
evanadwyer <evanadwyer@pronoid-software.com>
2024-07-24 09:14:41 -0700

    simplify spacing for visitor filter

--- agenda/presentation/src/main/java/com/pronoidsoftware/agenda/presentation/detail/components/event/visitor/components/EventDetailVisitorFilter.kt
+++ agenda/presentation/src/main/java/com/pronoidsoftware/agenda/presentation/detail/components/event/visitor/components/EventDetailVisitorFilter.kt
@@ -7,14 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -40,9 +38,6 @@ fun EventDetailVisitorFilter(
     modifier: Modifier = Modifier,
 ) {
     val spacing = LocalSpacing.current
-    val configuration = LocalConfiguration.current
-    val screenWidthDp = configuration.screenWidthDp.dp
-    val sizeHorizontal = (screenWidthDp - (spacing.spaceMedium * 4)) / 3
 
     Row(
         modifier = modifier
@@ -55,21 +50,21 @@ fun EventDetailVisitorFilter(
             selected = selectedFilterType == VisitorFilterType.ALL,
             onClick = onAllClick,
             modifier = Modifier
-                .width(sizeHorizontal),
+                .weight(1f),
         )
         VisitorFilterSelectionPill(
             text = stringResource(id = R.string.going),
             selected = selectedFilterType == VisitorFilterType.GOING,
             onClick = onGoingClick,
             modifier = Modifier
-                .width(sizeHorizontal),
+                .weight(1f),
         )
         VisitorFilterSelectionPill(
             text = stringResource(id = R.string.not_going),
             selected = selectedFilterType == VisitorFilterType.NOT_GOING,
             onClick = onNotGoingClick,
             modifier = Modifier
-                .width(sizeHorizontal),
+                .weight(1f),
         )
     }
 }
