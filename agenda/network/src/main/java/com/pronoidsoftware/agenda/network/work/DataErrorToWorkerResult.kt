209b71e7a41f1ca0d4a0d1e629d165e55a460afa
Evan A Dwyer <evanadwyer@pronoid-software.com>
2024-08-21 14:30:21 -0700

    upload event to remote server and save result to local db (#122)

---
+++ agenda/network/src/main/java/com/pronoidsoftware/agenda/network/work/DataErrorToWorkerResult.kt
@@ -0,0 +1,23 @@
+package com.pronoidsoftware.agenda.network.work
+
+import com.pronoidsoftware.core.domain.util.DataError
+
+fun DataError.toWorkerResult(): DataErrorWorkerResult {
+    return when (this) {
+        DataError.Local.DISK_FULL -> DataErrorWorkerResult.FAILURE
+        DataError.Network.REQUEST_TIMEOUT -> DataErrorWorkerResult.RETRY
+        DataError.Network.UNAUTHORIZED -> DataErrorWorkerResult.RETRY
+        DataError.Network.CONFLICT -> DataErrorWorkerResult.RETRY
+        DataError.Network.TOO_MANY_REQUESTS -> DataErrorWorkerResult.RETRY
+        DataError.Network.NO_INTERNET -> DataErrorWorkerResult.RETRY
+        DataError.Network.PAYLOAD_TOO_LARGE -> DataErrorWorkerResult.FAILURE
+        DataError.Network.SERVER_ERROR -> DataErrorWorkerResult.RETRY
+        DataError.Network.SERIALIZATION -> DataErrorWorkerResult.FAILURE
+        DataError.Network.UNKNOWN -> DataErrorWorkerResult.FAILURE
+    }
+}
+
+enum class DataErrorWorkerResult {
+    FAILURE,
+    RETRY,
+}
